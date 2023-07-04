a06adde015ecb278afbcbcef58673ec157e476cb
lemon-yun <62294818+lemon-yun@users.noreply.github.com>
2023-07-04 19:00:48 +0800

    Add files via upload

---
+++ 20230704/20230704_4.c
@@ -0,0 +1,15 @@
+
+
+//计算字符串长度
+
+#include <stdio.h>
+#include <string.h>//函数strlen需要引用此同文件
+
+int main()
+{
+	int len = strlen("abc");
+	printf("%d\n",  len);
+	printf("%d\n", strlen("abc"));
+	return 0;
+}
+
